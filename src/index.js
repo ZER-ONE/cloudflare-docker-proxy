f69d9358d5b4fb6fc4dfbddf794f3cd1c06c0e12
ZER-ONE <39043083+ZER-ONE@users.noreply.github.com>
2025-01-30 01:55:37 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker." + flyoung.online]: dockerHub,
+  ["quay." + flyoung.online]: "https://quay.io",
+  ["gcr." + flyoung.online]: "https://gcr.io",
+  ["k8s-gcr." + flyoung.online]: "https://k8s.gcr.io",
+  ["k8s." + flyoung.online]: "https://registry.k8s.io",
+  ["ghcr." + flyoung.online]: "https://ghcr.io",
+  ["cloudsmith." + flyoung.online]: "https://docker.cloudsmith.io",
+  ["ecr." + flyoung.online]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging." + flyoung.online]: dockerHub,
 };
 
 function routeByHosts(host) {
